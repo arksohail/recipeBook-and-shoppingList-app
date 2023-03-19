9575e12f9ce18e8dfc93059ca00aa43b816fa823
Shaikh Abdul Sohail <shaikh.sohail@i-exceed.com>
2023-03-20 04:41:49 +0530

    HTML, TS: Updated Shopping List and Edit Component's Controller and Template. Allowing the User to Add Ingredients to the Shopping List

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/model/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
+
+  @ViewChild('amountInput') amountInput : ElementRef;
+  @Output() ingredientCreated = new EventEmitter<Ingredient>();
+  onAdd(name: HTMLInputElement): void {
+    console.log(name.value);
+    console.log(this.amountInput.nativeElement.value);
+    if (name.value && this.amountInput.nativeElement.value) {
+      this.ingredientCreated.emit(new Ingredient(name.value, this.amountInput.nativeElement.value))
+    }
+  }
+  onDelete(): void {
+
+  }
+  onClear(nameInput): void {
+    this.amountInput.nativeElement.value = '';
+    nameInput.value = '';
+  }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -11,4 +11,8 @@ export class ShoppingListComponent {
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
   ];
+
+  onIngredientAdded(event: Ingredient): void {
+    this.ingredients.push(event)
+  }
 }
